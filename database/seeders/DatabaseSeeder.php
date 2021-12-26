ebce2365612bb76e964c7511760df73154dcac67
nooralight <2017100000041@seu.edu.bd>
2021-12-27 01:59:19 +0600

    init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user= User::create([
+            "username"=>"noorLight",
+            "email"=>"contactwnoor@gmail.com",
+            "password"=>"6984125o",
+        ]);
+
+
+        //save role
+
+        $role= $user->roles()->create([
+            "name"=>"ROLE_ADMIN",
+        ]);
+        $user->roles()->sync([$role->id]);
     }
 }
